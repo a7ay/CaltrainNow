7208890a10a5f20ce11d92adb6bd7c73efcc3280
Ajay Gupta <ajaygupta@gmail.com>
2026-02-23 19:53:08 -0800

    Fixed warnings.

--- app/src/main/java/com/caltrainnow/ui/MainActivity.kt
+++ app/src/main/java/com/caltrainnow/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
